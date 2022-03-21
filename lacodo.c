ca2b860ee11f22c2c4231be40756cb092774b787
Pedro Henrique <pehen10@gmail.com>
2022-03-21 11:59:37 -0300

    numeros pares entre 0 e 50

---
+++ lacodo.c
@@ -0,0 +1,13 @@
+//Programa para apresentar pares entre 0 e 50. Vamos usar a estrutura while
+#include <stdio.h>
+int main(){
+    int i = 0;
+    do{
+        if(i%2 == 0){
+            printf("O número %d é par\n",i);
+        }
+        i++;
+    }while(i<=50);
+    
+    return 0;
+}
