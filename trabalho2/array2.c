c53d78bfad9aa900c3afd9efe0a014b12193e492
Pedro Tateishi <pehen10@gmail.com>
2022-04-01 09:11:23 -0300

    atividade array

--- trabalho2/array2.c
+++ trabalho2/array2.c
@@ -8,4 +8,6 @@ void main(){
     soma(v,10);
     maiorValor(v,10);
     menorValor(v,10);
+    ordenar(v,10);
+    
 } 
--- trabalho2/calculos.h
+++ trabalho2/calculos.h
@@ -37,4 +37,7 @@ void ordenar(int valores[], int tamanho){
         }
         valores[i+1] = x;
     }
+    for(int p = 0 ; p < tamanho ; p++){
+        printf("%d - ",valores[p]);
+    }
 } 
